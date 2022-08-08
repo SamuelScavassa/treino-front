f76cf454e67652144b60b2426965726ff814a0ab
Samuel <87392424+SamuelScavassa@users.noreply.github.com>
2022-08-08 09:12:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('header', {
+    duration: 1000,
+});
+
+sr.reveal('.texto', {
+    delay: 375,
+    duration: 2000});
+
+sr.reveal('.img', {
+    rotate: {x: 0, y:80, z: 0},
+    delay: 1000,
+    duration: 2000
+});
+
+sr.reveal('.marca', {
+    duration: 1000,
+});
